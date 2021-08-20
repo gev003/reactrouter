2c4a139950b2abf95f1059f154bf421892242cbe
Gevorg Ghazaryan <gevorg.ghazaryan@gatsoft.am>
2021-08-20 19:18:11 +0400

    not ready

---
+++ src/itemDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ItemDetail(props) {
+  console.log("[props] ", props);
+  return (
+    <div>
+      <h1>Item</h1>
+    </div>
+  );
+}
---
+++ src/shop.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function Shop() {
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    let info = localStorage.getItem("userPosts");
+    if (!info) {
+      const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const response = await data.json();
+      setItems(response);
+      console.log(response);
+      localStorage.setItem("userPosts", JSON.stringify(response));
+    } else {
+      setItems(JSON.parse(info));
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  return (
+    <div>
+      <h1>Shop page</h1>
+      {items.map((obj) => {
+        return (
+          <div key={obj.id}>
+            <span>
+              <Link to={`/shop/${obj.id}`}>{obj.title}</Link>
+            </span>{" "}
+            <br />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Shop;
